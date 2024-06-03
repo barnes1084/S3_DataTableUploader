659f077842401b5aa885f784c51c1ebc601b011d
barnes1084 <120264291+barnes1084@users.noreply.github.com>
2024-06-03 13:08:03 -0400

    Update barcode_scan.sql

--- barcode_scan.sql
+++ barcode_scan.sql
@@ -1,5 +1,5 @@
 Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.1.1)(PORT=1521)))(CONNECT_DATA=(SID=th)));User Id=area2;Password=cuuwfxmgdgdldi2;
-
+Barcodes, CarrierId, Timestamp
 SELECT * 
 FROM BARCODEREADS 
 ORDER BY TIMESTAMP DESC
